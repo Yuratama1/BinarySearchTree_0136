5c4322d5e67ca82c62887b8042276283abdada93
Yuratama1 <yuratama661@gmail.com>
2025-06-26 15:02:24 +0700

    membuat constuctor untuk kelas node, dengan inisiasi leftchild rightchild ke nul

--- treee.cpp
+++ treee.cpp
@@ -6,3 +6,10 @@ class node{
         int info;
         node *leftchild;
         node *rightchild;
+
+        //constructor untuk class node
+        node(){
+            leftchild = nullptr; // inisiasi left child ke null
+            rightchild = nullptr;
+        }
+};
